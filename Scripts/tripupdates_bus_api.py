d0a53b09a706930d1d5521e4e213ffcae31ad308
vedsathwik275 <vmadabushi6@gatech.edu>
2025-02-19 21:23:14 -0500

    vehicle position and trip updates bus api added for realtime bus tracking.

---
+++ Scripts/tripupdates_bus_api.py
@@ -0,0 +1,10 @@
+from google.transit import gtfs_realtime_pb2
+import requests
+
+feed = gtfs_realtime_pb2.FeedMessage()
+response = requests.get('https://gtfs-rt.itsmarta.com/TMGTFSRealTimeWebService/tripupdate/tripupdates.pb')
+feed.ParseFromString(response.content)
+print(feed)
+# for entity in feed.entity:
+#   if entity.HasField('vehicle'):
+#     print(entity.vehicle)
